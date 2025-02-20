b65ba0d198b17b0466cc50ebc9593cba9ea7e36a
Noarbre <124240607+NoaChenel@users.noreply.github.com>
2025-02-20 10:05:59 +0100

    matrice des distances
    génération aléatoire des points ainsi que le calcul entre ces point rangé dans une matrice

---
+++ Algo Noarbre/Exploration/Instance.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+class Instance
+{
+    private int[,] mat_dis;
+    private int[,] lis_point;
+    private int taille;
+
+    public int[,] Mat_dis
+    {
+        get
+        {
+            return this.mat_dis;
+        }
+    }
+
+    public Instance(int nb)
+    {
+        this.mat_dis = new int[nb,nb];
+        this.lis_point = new int[nb,2];
+        this.taille = nb;
+        RemplirMatrice();
+    }
+
+    private bool contient(int x, int y)
+    {
+        for(int i=0; i<this.taille; ++i)
+        {
+            if (this.lis_point[i,0] == x || this.lis_point[i,1] == y) return true;
+        }
+        return false;
+    }
+
+    private void RemplirMatrice()
+    {
+        Random rnd = new Random();
+        for(int i=0; i<this.taille; ++i)
+        {
+            int x;
+            int y;
+            do
+            {
+                x = rnd.Next(1000);
+                y = rnd.Next(1000);
+            } while (contient(x, y));
+            this.lis_point[i, 0] = x;
+            this.lis_point[i, 1] = y;
+        }
+
+        for(int i=0; i<this.taille; ++i)
+        {
+            for(int j=0; j<this.taille; ++j)
+            {
+                if (i == j) this.mat_dis[i, j] = 0;
+                else this.mat_dis[i, j] = distance(this.lis_point[i, 0], this.lis_point[j, 0], this.lis_point[i, 1], this.lis_point[j, 1]);
+            }
+        }
+    }
+
+    private int distance(int x1, int x2, int y1, int y2)
+    {
+        return (int)(Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+    }
+
+    public void affiche()
+    {
+        for (int i = 0; i < this.taille; ++i)
+        {
+            Console.Write(this.lis_point[i, 0].ToString() + "-" + this.lis_point[i, 1].ToString() + " | ");
+        }
+        Console.WriteLine();
+
+            for (int i = 0; i < this.taille; ++i)
+        {
+            for (int j = 0; j < this.taille; ++j)
+            {
+                Console.Write(this.mat_dis[i, j].ToString() + " | ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Algo Noarbre/Exploration/Program.cs
+++ Algo Noarbre/Exploration/Program.cs
@@ -1,1 +1,10 @@
 ﻿using System;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Instance test = new Instance(10);
+        test.affiche();
+    }
+}
