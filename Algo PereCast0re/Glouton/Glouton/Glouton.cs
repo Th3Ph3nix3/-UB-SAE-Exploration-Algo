4c5d936f1453906458f5277c27075c66c3e3f11d
Père Castore <barthoux4@gmail.com>
2025-04-16 16:09:07 +0200

    Start Glouton
    Début du dev de l'algo glouton

---
+++ Algo PereCast0re/Glouton/Glouton/Glouton.cs
@@ -0,0 +1,39 @@
+using System;
+
+public class Glouton {
+	private int depart;
+	public int Depart {
+		get {
+			return depart;
+		}
+		set {
+			depart = value;
+		}
+	}
+	private int[] cheminGlouton;
+	public int[] CheminGlouton {
+		get {
+			return cheminGlouton;
+		}
+		set {
+			cheminGlouton = value;
+		}
+	}
+
+	public Glouton(int depart, int[] cheminGlouton) {
+		
+	}
+	public void AddVisite(int point) {
+		throw new System.NotImplementedException("Not implemented");
+	}
+	public void AddChemin(int parameter) {
+		throw new System.NotImplementedException("Not implemented");
+	}
+	public void Parcour(int depart) {
+		throw new System.NotImplementedException("Not implemented");
+	}
+	public void ToString() {
+		throw new System.NotImplementedException("Not implemented");
+	}
+
+}
