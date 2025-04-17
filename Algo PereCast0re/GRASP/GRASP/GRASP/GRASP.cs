e8fd4f33075d5b6c6f7632d0aebc1d9063592af1
Père Castore <barthoux4@gmail.com>
2025-04-17 18:33:37 +0200

    GRASP effectué mais sous reserve
    Algo Grasp effectuer et fonctionnel mais a faire valider par le professeur car petit doute sur le respect de l'idée du principe de cette algo

---
+++ Algo PereCast0re/GRASP/GRASP/GRASP/GRASP.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace GRASP
+{
+    public class GRASP
+    {
+        #region attributes
+        private int depart;
+        private List<int> cheminGrasp;
+        #endregion
+
+        #region constructor
+        /// <summary>
+        /// Propriété CheminGrasp
+        /// </summary>
+        public List<int> CheminGrasp
+        {
+            get { return cheminGrasp; } 
+            set { cheminGrasp = value; } 
+        }
+
+        /// <summary>
+        /// Propriété Depart
+        /// </summary>
+        public int Depart
+        {
+            get { return depart; }
+            set { depart = value; }
+        }
+
+        /// <summary>
+        /// Constructeur de la classe GRASP
+        /// </summary>
+        /// <param name="depart"></param>
+        public GRASP(int[,] matrice)
+        {
+            this.depart = 0;
+            this.cheminGrasp = new List<int>(matrice.GetLength(0));
+        }
+        #endregion
+
+
+        #region methods
+        /// <summary>
+        /// Ajoute un point au chemin Grasp
+        /// </summary>
+        /// <param name="point"></param>
+        public void AddChemin(int point)
+        {
+            this.CheminGrasp.Add(point);
+        }
+
+        /// <summary>
+        /// Compter le nombre total de points dans la matrice
+        /// </summary>
+        /// <param name="matrice"></param>
+        /// <returns></returns>
+        public int Highest(int[,] matrice)
+        {
+            int maxi = int.MinValue;
+            for (int i = 0; i < matrice.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrice.GetLength(1); j++)
+                {
+                     if (maxi < matrice[i, j])
+                    {
+                        maxi = matrice[i, j];
+                    }
+                }
+            }
+            return maxi;
+        }
+
+        /// <summary>
+        /// Sélectionne un point aléatoire dans la matrice
+        /// </summary>
+        /// <param name="matrice"></param>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public int selectPoint(int[,] matrice, int point)
+        {
+            int calcul = 0;
+            int maxi = int.MaxValue;
+
+            //Création d'une liste de point
+            List<(int,int)> liste = new List<(int,int)>();
+            for (int i = 0; i < matrice.GetLength(0); i++)
+            {
+                liste.Add((i, matrice[point, i]));
+            }
+
+            Random random = new Random();
+            int rdm = random.Next(1, liste.Count);
+
+            // On cherche un point qui - le random et inférieur a 0 et on renvoie cette valeur 
+            for (int j = 0; j < liste.Count; j++)
+            {
+
+                calcul = liste[j].Item2 - rdm;
+                if (calcul < 0 && !IsVisited(liste[j].Item1))
+                {
+                    point = liste[j].Item1;
+                    break;
+                }
+                else
+                {
+                    if (calcul < maxi && !IsVisited(liste[j].Item1))
+                    {
+                        maxi = calcul;
+                        point = liste[j].Item1;
+                    }
+                }
+            }
+            return point;
+        }
+
+        /// <summary>
+        /// Vérifie si le point a déjà été visité
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public bool IsVisited(int point)
+        {
+            return this.cheminGrasp.Contains(point);
+        }
+
+        /// <summary>
+        /// Parcours la matrice et ajoute les points au chemin Grasp
+        /// </summary>
+        /// <param name="matrice"></param>
+        public void Parcour(int[,] matrice)
+        {
+            int point = this.depart;
+            int cout = 0;
+
+            AddChemin(point);
+
+            while (cheminGrasp.Count < matrice.GetLength(0))
+            {
+                int suivant = selectPoint(matrice, point);
+                cheminGrasp.Add(suivant);
+
+                cout += matrice[point, suivant];
+                point = suivant;
+            }
+
+            Console.WriteLine("Le cout du chemin est de : " + cout);
+
+        }
+
+        /// <summary>
+        /// Affiche le chemin grasp
+        /// </summary>
+        public void ToString()
+        {
+            int taille = this.CheminGrasp.Count;
+            string chemin = "";
+            for (int i = 0; i < taille; i++)    
+            {
+                chemin += this.CheminGrasp[i];
+                if (i < taille - 1)
+                {
+                    chemin += " -> ";
+                }
+            }
+            string message = "Le chemin glouton est : " + chemin;
+
+            Console.WriteLine(message);
+        }
+
+
+        #endregion
+
+    }
+}
